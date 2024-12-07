397c4323ddc21a2ec35b83cd36103f8708c9b933
Monodad <a5710168@gmail.com>
2024-12-08 02:31:00 +0800

    Added the Depthwise convolution

--- CNN.py
+++ CNN.py
@@ -6,10 +6,66 @@ import time
 import torch.nn.functional as f
 # Trying to use for loop to implement 3D convolution.
 # 16x112x112 image with 3 channel(R-G-B)
-input_data = torch.randn(3, 16, 112, 112)
-input_weight = torch.randn(64, 3, 3, 3, 3)
+
 # input_data = torch.randn(3, 16)
 # def cal_conv3D(input:Tensor,feature:Tensor,)
+
+
+def Depthwise(input: Tensor, filter: Tensor, Padding: int) -> Tensor:
+    # Do the zero padding
+
+    padding = nn.ZeroPad3d(1)
+    input = padding(input)
+    # Get the size and change the format to the numpy
+    input_size = input.size()
+    filter_size = filter.size()
+    input = input.numpy()
+    filter = filter.numpy()
+    init_array = False
+    if filter_size[0] != input_size[0]:
+        print("Filter channel not equal to input channel!")
+        return torch.randn(0)
+
+        # calculate the convolution and save the parameter
+    output = []
+    for channel in range(0, input_size[0]):
+        out_height_arr = []
+        for input_depth in range(0, input_size[1] - 2):
+            out_width_arr = []
+            for input_height in range(0, input_size[2] - 2):
+                out_depth_arr = []
+                for input_width in range(0, input_size[3]-2):
+                    result = 0
+                    for filter_depth in range(0, filter_size[1]):
+                        for filter_height in range(0, filter_size[2]):
+                            for filter_width in range(0, filter_size[3]):
+                                result = result + \
+                                    input[channel, input_depth+filter_depth, input_height + filter_height, input_width+filter_width] * \
+                                    filter[channel, filter_depth,
+                                           filter_height, filter_width]
+                    out_depth_arr = np.append(
+                        out_depth_arr, result)
+                    # print(out_depth_arr)
+                    # [3,3,3
+
+                if len(out_width_arr) == 0:
+                    out_width_arr = np.expand_dims(out_depth_arr, axis=0)
+                else:
+                    out_width_arr = np.concatenate(
+                        [out_width_arr, np.expand_dims(out_depth_arr, axis=0)], axis=0)
+
+            # [[3,3,3],[3,3,3]] 3x2
+            if len(out_height_arr) == 0:
+                out_height_arr = np.expand_dims(out_width_arr, axis=0)
+            else:
+                out_height_arr = np.concatenate(
+                    [out_height_arr, np.expand_dims(out_width_arr, axis=0)], axis=0)
+        if len(output) == 0:
+            output = np.expand_dims(out_height_arr, axis=0)
+        else:
+            output = np.concatenate([output, np.expand_dims(
+                out_height_arr, axis=0)], axis=0)
+    return (torch.from_numpy(output))
 
 
 def conv3D(input: Tensor, weight: Tensor, bias: Tensor) -> Tensor:
@@ -86,11 +142,11 @@ def conv3D(input: Tensor, weight: Tensor, bias: Tensor) -> Tensor:
 
 
 #   torch.Size([64, 3, 3, 3, 3])
-#   ([input channel,out_channels,kernel_depth,kernel_height,kernel_width])
+# #   ([input channel,out_channels,kernel_depth,kernel_height,kernel_width])
 # Parm_file = "../C3D-main_DSC/models/c3d-pretrained.pth"
 # model = torch.load(Parm_file, weights_only=False)
-# bias = 0
-# feature = 0
+# # bias = 0
+# # feature = 0
 # output = 0
 # first_input = 0
 # get_weight = 0
@@ -123,4 +179,12 @@ def conv3D(input: Tensor, weight: Tensor, bias: Tensor) -> Tensor:
 #         get_bias = 0
 
 if __name__ == "__main__":
-    conv3D(input_data, input_weight, input_data)
+    # conv3D(input_data, input_weight, input_data)
+    # data = Depthwise(input_data, DWC_filter, 1)
+    input_data = torch.randn(3, 16, 112, 112)
+    input_weight = torch.randn(64, 3, 3, 3, 3)
+    DWC_filter = torch.randn(3, 3, 3, 3)
+    print("Running...")
+    output = Depthwise(input_data, DWC_filter, 1)
+    output = f.max_pool3d(output, kernel_size=(1, 2, 2), stride=(1, 2, 2))
+    print(output.size())
